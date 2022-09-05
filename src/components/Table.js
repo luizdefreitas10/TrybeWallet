fb14e8e213677a8b5ceb36841816c56ccc9fb1cb
Luiz de Freitase <luizdefreitas10@gmail.com>
2022-09-05 17:53:50 -0300

    att css

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { deleteExpenseAction, editExpenseAction } from '../redux/actions/index';
 
+import './Table.css';
+
 class Table extends Component {
   handleDelete = (id) => {
     const { dispatchId } = this.props;
@@ -20,58 +22,60 @@ class Table extends Component {
   render() {
     const { getFormState } = this.props;
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
-        </thead>
-        { getFormState.map((expense) => (
-          <tbody key={ expense.id }>
+      <div className="div-table-class">
+        <table>
+          <thead>
             <tr>
-              <td>{ expense.description }</td>
-              <td>{ expense.tag }</td>
-              <td>{ expense.method }</td>
-              <td>{ Number(expense.value).toFixed(2) }</td>
-              <td>{ expense.exchangeRates[expense.currency].name }</td>
-              <td>{ Number(expense.exchangeRates[expense.currency].ask).toFixed(2) }</td>
-              <td>
-                { (Number(expense.value) * (expense
-                  .exchangeRates[expense.currency].ask)).toFixed(2) }
-              </td>
-              <td>Real</td>
-              <td>
-
-                <button
-                  type="button"
-                  data-testid="delete-btn"
-                  onClick={ () => this.handleDelete(expense.id) }
-                >
-                  Excluir
-                </button>
-
-                <button
-                  type="button"
-                  data-testid="edit-btn"
-                  onClick={ () => this.handleEdit(expense.id) }
-                >
-                  Editar
-                </button>
-
-              </td>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
             </tr>
-          </tbody>
-        )) }
+          </thead>
+          { getFormState.map((expense) => (
+            <tbody key={ expense.id }>
+              <tr>
+                <td>{ expense.description }</td>
+                <td>{ expense.tag }</td>
+                <td>{ expense.method }</td>
+                <td>{ Number(expense.value).toFixed(2) }</td>
+                <td>{ expense.exchangeRates[expense.currency].name }</td>
+                <td>{ Number(expense.exchangeRates[expense.currency].ask).toFixed(2) }</td>
+                <td>
+                  { (Number(expense.value) * (expense
+                    .exchangeRates[expense.currency].ask)).toFixed(2) }
+                </td>
+                <td>Real</td>
+                <td>
 
-      </table>
+                  <button
+                    type="button"
+                    data-testid="delete-btn"
+                    onClick={ () => this.handleDelete(expense.id) }
+                  >
+                    Excluir
+                  </button>
+
+                  <button
+                    type="button"
+                    data-testid="edit-btn"
+                    onClick={ () => this.handleEdit(expense.id) }
+                  >
+                    Editar
+                  </button>
+
+                </td>
+              </tr>
+            </tbody>
+          )) }
+
+        </table>
+      </div>
     );
   }
 }
